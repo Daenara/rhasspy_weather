4f4a62b304da7516c75592b288fe1113c3ff1261
Daenara <nadine@daenara.net>
2020-03-26 21:18:08 +0100

    fixed a small bug

--- data_types/location.py
+++ data_types/location.py
@@ -2,6 +2,7 @@
 class Location:
     def __init__(self, city):
         self.city = city
+        self.name = city # used for output only, intended for custom queries like how is the weather at grandmas (not implemented yet)
 
     def set_lat_and_lon(self, lat, lon):
         self.lat = float(lat)
