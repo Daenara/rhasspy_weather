3d1af49e982a1bace825a1e890312316cf528f29
Daenara <nadine@daenara.net>
2020-08-10 00:11:54 +0200

    fixed times so they actually cover the complete day and don't overlap

--- languages/german.py
+++ languages/german.py
@@ -16,9 +16,9 @@ named_times = {
     "Morgen": (datetime.time(6, 0), datetime.time(10, 0)),
     "Vormittag": (datetime.time(10, 0), datetime.time(12, 0)),
     "Mittag": (datetime.time(12, 0), datetime.time(14, 0)),
-    "Nachmittag": (datetime.time(15, 0), datetime.time(18, 0)),
+    "Nachmittag": (datetime.time(14, 0), datetime.time(18, 0)),
     "Abend": (datetime.time(18, 0), datetime.time(22, 0)),
-    "Nacht": (datetime.time(22, 0), datetime.time(7, 0))
+    "Nacht": (datetime.time(22, 0), datetime.time(6, 0))
 }
 named_times_synonyms = {
     "früh": "Morgen"
