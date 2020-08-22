747b13b870b410ce0912763e712bbb36a7a983a0
Daenara <nadine@daenara.net>
2020-08-22 20:36:13 +0200

    added more tests

--- tests/test_dt_utils.py
+++ tests/test_dt_utils.py
@@ -2,7 +2,7 @@ import datetime
 
 from rhasspy_weather.data_types.config import get_config
 
-from rhasspy_weather.utils.dt_utils import get_date_with_year
+from rhasspy_weather.utils.dt_utils import get_date_with_year, named_day_to_date, weekday_to_date, date_string_to_date
 
 
 def test_get_date_with_year(mock_config_detail_true):
@@ -29,3 +29,49 @@ def test_get_date_with_year(mock_config_detail_true):
     assert test_4.month == today.month
     assert test_4.year == today.year
 
+
+def test_named_day_to_date(mock_config_detail_true):
+    config = get_config()
+    today = datetime.datetime.now(tz=config.timezone).date()
+    for named_day in config.locale.named_days.keys():
+        result = named_day_to_date(named_day)
+        named_day_value = config.locale.named_days[named_day]
+        if type(named_day_value) == int:
+            offset = datetime.timedelta(named_day_value)
+            assert today + offset == result
+        elif type(named_day_value) == datetime.date:
+            assert named_day_value == result
+
+    for named_day in config.locale.named_days_synonyms.keys():
+        result = named_day_to_date(named_day)
+        named_day_value = config.locale.named_days[config.locale.named_days_synonyms[named_day]]
+        if type(named_day_value) == int:
+            offset = datetime.timedelta(named_day_value)
+            assert today + offset == result
+        elif type(named_day_value) == datetime.date:
+            assert named_day_value == result
+
+
+def test_weekday_to_date(mock_config_detail_true):
+    config = get_config()
+    for weekday in config.locale.weekday_names:
+        result = weekday_to_date(weekday)
+        assert result.weekday() == config.locale.weekday_names.index(weekday)
+
+    today = datetime.datetime.now(tz=config.timezone).date()
+    assert weekday_to_date(config.locale.weekday_names[today.weekday()]) == today + datetime.timedelta(days=7)
+
+
+def test_date_string_to_date(mock_config_detail_true):
+    config = get_config()
+    result = date_string_to_date("31 " + config.locale.month_names[4])
+    assert result.day == 31
+    assert result.month == 5
+
+    result = date_string_to_date("31 5")
+    assert result.day == 31
+    assert result.month == 5
+
+    result = date_string_to_date("31:05", ":")
+    assert result.day == 31
+    assert result.month == 5
