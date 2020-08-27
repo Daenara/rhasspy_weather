4dbf4ac5ff277bc0d01f6dd59ca0091686daa21a
Nadine Seibel <nadine.seibel@googlemail.com>
2020-08-27 13:28:04 +0200

    added output option to write to log

---
+++ rhasspy_weather/output/log.py
@@ -0,0 +1,12 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def output_response(output):
+    log.debug("Selected output: console_json")
+    log.info(output)
+
+
+def parse_config(config_parser):
+    pass
