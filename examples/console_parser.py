61c063d5a915a2e76ec82c832c9d978882d66a2e
Nadine Seibel <nadine.seibel@googlemail.com>
2020-09-02 16:28:16 +0200

    added commandline support (thanks to ulno for allowing me to use his code for it)

---
+++ examples/console_parser.py
@@ -0,0 +1,45 @@
+# thanks to ulno who wrote the console parser for rhasspy_weather (https://github.com/ulno/cli_weather) that I used as an example
+
+import argparse
+import json
+import logging
+import os
+import sys
+
+from custom_logger import custom_logger
+from rhasspy_weather.weather import get_weather_forecast
+
+
+logfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'console_output.log')
+root_logger = custom_logger(logfile)
+log = logging.getLogger(__name__)
+
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--day', help='Forecast day (sunday, monday, ...) or "day month".')  # when_day
+    parser.add_argument('-t', '--time', help='Forecast time')  # when_time
+    parser.add_argument('-l', '--location', help='Forecast location')  # location
+    parser.add_argument('-i', '--item', help='Is a specific item (like umbrella) needed/recommended.')  # item
+    parser.add_argument('-c', '--condition', help='Is a specific condition active at given time.')  # condition
+    parser.add_argument('-e', '--temperature', help='Temperature forecast.')  # temperature
+    parser.add_argument('-f', '--configfile', help="Path to the config file")
+    parser.add_argument('-j', '--json', help="Receive json in rhasspy intent event format as one parameter string or via stdin when this is set to a dash (-) and forward that to rhasspy_weather component.")
+
+    args = parser.parse_args()
+    config_path = None
+    if args.configfile is not None:
+        config_path = args.configfile
+    weather_input = args
+    if args.json is not None:
+        if args.json == "-":
+            # read and parse json from stdin and send it to rhasspy_weather
+            weather_input = json.load(sys.stdin)
+        else:
+            weather_input = json.loads(args.json)
+
+    print(get_weather_forecast(weather_input, config_path=config_path))
+
+
+if __name__ == '__main__':
+    parse()
---
+++ rhasspy_weather/parser/console_args.py
@@ -0,0 +1,125 @@
+import datetime
+import json
+import logging
+
+from rhasspy_weather.data_types.request import WeatherRequest, DateType, ForecastType, Grain
+from rhasspy_weather.data_types.config import get_config
+from rhasspy_weather.utils.parser import parse_date, parse_time, parse_condition, parse_item, parse_temperature, parse_location
+
+log = logging.getLogger(__name__)
+
+
+def parse_intent_message(args: json) -> WeatherRequest:
+    """
+    Parses any of the rhasspy weather intents.
+
+    Args:
+        args: dict containing the arguments
+
+    Returns: WeatherRequest object
+
+    """
+    if args.condition is not None:
+        return parse_condition_intent(args)
+    elif args.item is not None:
+        return parse_item_intent(args)
+    elif args.temperature is not None:
+        return parse_temperature_intent(args)
+
+    return parse_general_intent(args)
+
+
+def parse_general_intent(args:json) -> WeatherRequest:
+    """
+    Parses general rhasspy weather intents.
+
+    Args:
+        args: the rhasspy intent message
+
+    Returns: WeatherRequest object
+
+    """
+    config = get_config()
+    today = datetime.datetime.now(config.timezone).date()
+
+    # define default request
+    new_request = WeatherRequest(DateType.FIXED, Grain.DAY, today, ForecastType.FULL)
+
+    arg_day = args.day
+    if arg_day is not None:
+        new_request.request_date, new_request.date_specified = parse_date(arg_day, config.locale)
+
+    arg_time = args.time
+    if arg_time is not None:
+        time, str_time = parse_time(arg_time, config.locale)
+        if isinstance(time, tuple):
+            new_request.date_type = DateType.INTERVAL
+            new_request.start_time, new_request.end_time = time
+        new_request.time_specified = str_time
+        new_request.grain = Grain.HOUR
+
+    arg_location = args.location
+    if arg_location is not None:
+        new_request.location = parse_location(arg_location, config.locale)
+
+    return new_request
+
+
+def parse_condition_intent(args: json) -> WeatherRequest:
+    """
+    Parses rhasspy condition weather intent.
+
+    Args:
+        args: the rhasspy intent message
+
+    Returns: WeatherRequest object
+
+    """
+    locale = get_config().locale
+    new_request = parse_general_intent(args)
+
+    new_request.forecast_type = ForecastType.CONDITION
+    arg_condition = args.condition
+    if arg_condition is not None:
+        new_request.requested = parse_condition(arg_condition, locale)
+    return new_request
+
+
+def parse_item_intent(args: json) -> WeatherRequest:
+    """
+    Parses rhasspy item weather intent
+
+    Args:
+        args: the rhasspy intent message
+
+    Returns: WeatherRequest object
+
+    """
+    locale = get_config().locale
+    new_request = parse_general_intent(args)
+
+    new_request.forecast_type = ForecastType.ITEM
+    arg_item = args.item
+    if arg_item is not None:
+        new_request.requested = parse_item(arg_item, locale)
+    return new_request
+
+
+def parse_temperature_intent(args: json) -> WeatherRequest:
+    """
+    Parses rhasspy temperature weather intent
+
+    Args:
+        args: the rhasspy intent message
+
+    Returns: WeatherRequest object
+
+    """
+    locale = get_config().locale
+    new_request = parse_general_intent(args)
+
+    new_request.forecast_type = ForecastType.TEMPERATURE
+    arg_temperature = args.temperature
+    if arg_temperature is not None:
+        new_request.requested = parse_temperature(arg_temperature, locale)
+    return new_request
