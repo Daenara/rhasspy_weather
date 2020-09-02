967199f272e0b307f639185d577881c47f1f8b78
Nadine Seibel <nadine.seibel@googlemail.com>
2020-09-02 15:34:49 +0200

    moved most logic of rhasspy_intent to a new parser util class so it can be reused in other parsers

--- rhasspy_weather/parser/rhasspy_intent.py
+++ rhasspy_weather/parser/rhasspy_intent.py
@@ -1,12 +1,10 @@
 import datetime
 import logging
 
-from rhasspy_weather.utils import dt_utils as dt_utils
-from rhasspy_weather.data_types.condition import ConditionType
-from rhasspy_weather.data_types.error import WeatherError, ErrorCode
-from rhasspy_weather.data_types.location import Location
 from rhasspy_weather.data_types.request import WeatherRequest, DateType, ForecastType, Grain
 from rhasspy_weather.data_types.config import get_config
+from rhasspy_weather.utils.parser import parse_date, parse_time, parse_condition, parse_temperature, parse_item, \
+    parse_location
 
 log = logging.getLogger(__name__)
 
@@ -59,26 +57,19 @@ def parse_general_intent(intent_message: dict) -> WeatherRequest:
 
     slots = intent_message["slots"]
 
-    # if a day was specified
-    log.debug("parse_date")
     if slot_names["day"] in slots and slots[slot_names["day"]] != "":
-        new_request.request_date, new_request.date_specified = parse_date(slots, config.locale)
+        new_request.request_date, new_request.date_specified = parse_date(slots[slot_names["day"]], config.locale)
 
-    # if a time was specified
-    log.debug("parse_time")
     if slot_names["time"] in slots and slots[slot_names["time"]] != "":
-        time, str_time = parse_time(slots, config.locale)
+        time, str_time = parse_time(slots[slot_names["time"]], config.locale)
         if isinstance(time, tuple):
             new_request.date_type = DateType.INTERVAL
             new_request.start_time, new_request.end_time = time
         new_request.time_specified = str_time
         new_request.grain = Grain.HOUR
 
-    # location
-    log.debug("parse_location")
     if slot_names["location"] in slots and slots[slot_names["location"]] != "":
-        log.debug("a location was specified")
-        new_request.location = Location(slots[slot_names["location"]])
+        new_request.location = parse_location(slots[slot_names["location"]], config.locale)
 
     return new_request
 
@@ -99,12 +90,7 @@ def parse_condition_intent(intent_message: dict) -> WeatherRequest:
     slots = intent_message["slots"]
     new_request.forecast_type = ForecastType.CONDITION
     if slot_names["condition"] in slots:
-        if slots[slot_names["condition"]] in locale.condition_types:
-            new_request.requested = locale.condition_types[slots[slot_names["condition"]]]
-        elif slots[slot_names["condition"]] in locale.condition_synonyms:
-            new_request.requested = locale.condition_types[locale.condition_synonyms[slots[slot_names["condition"]]]]
-        else:
-            new_request.requested = ConditionType.UNKNOWN
+        new_request.requested = parse_condition(slots[slot_names["condition"]], locale)
     return new_request
 
 
@@ -124,10 +110,7 @@ def parse_item_intent(intent_message: dict) -> WeatherRequest:
     slots = intent_message["slots"]
     new_request.forecast_type = ForecastType.ITEM
     if slot_names["item"] in slots:
-        items = locale.rain_items + locale.cold_items + locale.warm_items + locale.sun_items
-        items_lowercase = [x.lower() for x in items]
-        if slots[slot_names["item"]].lower() in items_lowercase:
-            new_request.requested = items[items_lowercase.index(slots[slot_names["item"]].lower())]
+        new_request.requested = parse_item(slots[slot_names["item"]], locale)
     return new_request
 
 
@@ -147,65 +130,5 @@ def parse_temperature_intent(intent_message: dict) -> WeatherRequest:
     slots = intent_message["slots"]
     new_request.forecast_type = ForecastType.TEMPERATURE
     if slot_names["temperature"] in slots:
-        if slots[slot_names["temperature"]] in locale.temperature_types:
-            new_request.requested = locale.temperature_types[slots[slot_names["temperature"]]]
-        elif slots[slot_names["temperature"]] in locale.temperature_synonyms:
-            new_request.requested = locale.temperature_types[locale.temperature_synonyms[slots[slot_names["temperature"]]]]
+        new_request.requested = parse_temperature(slots[slot_names["temperature"]], locale)
     return new_request
-
-
-def parse_date(slots, locale):
-    log.debug("intent contains a specified day")
-
-    named_days_lowercase = [x.lower() for x in locale.named_days.keys()]
-    named_days_synonyms_lowercase = [x.lower() for x in locale.named_days_synonyms.keys()]
-    # is it a named day (tomorrow, etc.)?
-    if slots[slot_names["day"]].lower() in named_days_lowercase + named_days_synonyms_lowercase:
-        log.debug("date is specified by name")
-        return dt_utils.named_day_to_date(slots[slot_names["day"]]), dt_utils.named_day_to_str(slots[slot_names["day"]])
-
-    # is a weekday named?
-    weekdays_lowercase = [x.lower() for x in locale.weekday_names]
-    if slots[slot_names["day"]].lower() in weekdays_lowercase:
-        log.debug("date is specified by weekday name")
-        new_date = dt_utils.weekday_to_date(slots[slot_names["day"]].lower())
-        return new_date, weekdays_lowercase[new_date.weekday()]
-
-    # was a date specified (specified by rhasspy as "daynumber monthname")?
-    if ' ' in slots[slot_names["day"]]:
-        log.debug("date was specified in form 'day month'")
-        return dt_utils.date_string_to_date(slots[slot_names["day"]]), dt_utils.date_string_to_str(slots[slot_names["day"]])
-
-    log.error("Unknown date format")
-    raise WeatherError(ErrorCode.DATE_ERROR)
-
-
-def parse_time(slots, locale):
-    if slot_names["time"] in slots and slots[slot_names["time"]] != "":
-        log.debug("intent contains a specified time")
-
-        if isinstance(slots[slot_names["time"]], str):
-            named_times_lowercase = [x.lower() for x in locale.named_times.keys()]
-            named_times_synonyms_lowercase = [x.lower() for x in locale.named_times_synonyms.keys()]
-
-            # was something like midday specified (listed in locale.named_times or in locale.named_times_synonyms)?
-            if slots[slot_names["time"]].lower() in named_times_lowercase + named_times_synonyms_lowercase:
-                log.debug("time is specified by name")
-                return dt_utils.named_time_to_time(slots[slot_names["time"]]), dt_utils.named_day_to_str(slots[slot_names["time"]])
-
-            # was it hours and minutes (specified as "HH MM" by rhasspy intent)?
-            if ' ' in slots[slot_names["time"]]:
-                log.debug("time was specified in form 'HH MM'")
-                time = datetime.datetime.strptime(slots[slot_names["time"]], '%H %M').time()
-                return time, locale.format_userdefined_time(time.hour, time.minute)
-
-            log.error("Unknown format for time string")
-            raise WeatherError(ErrorCode.TIME_ERROR)
-        # is it only an hour (no way to only specify minutes, if it is an int, it is hours)?
-        elif isinstance(slots[slot_names["time"]], int):
-            log.debug("time specified was hours (as a number)")
-            time = datetime.time(slots[slot_names["time"]], 0)
-            return time, locale.format_userdefined_time(time.hour)
-        else:
-            log.error("Unknown time format")
-            raise WeatherError(ErrorCode.TIME_ERROR)
---
+++ rhasspy_weather/utils/parser.py
@@ -0,0 +1,102 @@
+import datetime
+from typing import TypeVar
+
+from rhasspy_weather.data_types.condition import ConditionType
+from rhasspy_weather.data_types.error import WeatherError, ErrorCode
+from rhasspy_weather.data_types.location import Location
+from rhasspy_weather.utils import dt_utils
+import logging
+
+log = logging.getLogger(__name__)
+
+InputTime = TypeVar("InputTime", str, int)
+
+
+def parse_date(date: str, locale):
+    log.debug(f"parse date {date}")
+
+    named_days_lowercase = [x.lower() for x in locale.named_days.keys()]
+    named_days_synonyms_lowercase = [x.lower() for x in locale.named_days_synonyms.keys()]
+    # is it a named day (tomorrow, etc.)?
+    if date.lower() in named_days_lowercase + named_days_synonyms_lowercase:
+        log.debug("date is specified by name")
+        return dt_utils.named_day_to_date(date), dt_utils.named_day_to_str(date)
+
+    # is a weekday named?
+    weekdays_lowercase = [x.lower() for x in locale.weekday_names]
+    if date.lower() in weekdays_lowercase:
+        log.debug("date is specified by weekday name")
+        new_date = dt_utils.weekday_to_date(date.lower())
+        return new_date, weekdays_lowercase[new_date.weekday()]
+
+    # was a date specified (specified by rhasspy as "daynumber monthname")?
+    if ' ' in date:
+        log.debug("date was specified in form 'day month'")
+        return dt_utils.date_string_to_date(date), dt_utils.date_string_to_str(date)
+
+    log.error("Unknown date format")
+    raise WeatherError(ErrorCode.DATE_ERROR)
+
+
+def parse_time(time: InputTime, locale):
+    log.debug(f"parse time - {time}")
+    if time != "":
+        log.debug("intent contains a specified time")
+
+        if isinstance(time, str):
+            named_times_lowercase = [x.lower() for x in locale.named_times.keys()]
+            named_times_synonyms_lowercase = [x.lower() for x in locale.named_times_synonyms.keys()]
+
+            # was something like midday specified (listed in locale.named_times or in locale.named_times_synonyms)?
+            if time.lower() in named_times_lowercase + named_times_synonyms_lowercase:
+                log.debug("time is specified by name")
+                return dt_utils.named_time_to_time(time), dt_utils.named_day_to_str(time)
+
+            # was it hours and minutes (specified as "HH MM" by rhasspy intent)?
+            if ' ' in time:
+                log.debug("time was specified in form 'HH MM'")
+                time = datetime.datetime.strptime(time, '%H %M').time()
+                return time, locale.format_userdefined_time(time.hour, time.minute)
+
+            log.error("Unknown format for time string")
+            raise WeatherError(ErrorCode.TIME_ERROR)
+        # is it only an hour (no way to only specify minutes, if it is an int, it is hours)?
+        elif isinstance(time, int):
+            log.debug("time specified was hours (as a number)")
+            time = datetime.time(time, 0)
+            return time, locale.format_userdefined_time(time.hour)
+        else:
+            log.error("Unknown time format")
+            raise WeatherError(ErrorCode.TIME_ERROR)
+
+
+def parse_location(location: str, locale):
+    log.debug(f"parse location - {location}")
+    return Location(location)
+
+
+def parse_condition(condition: str, locale):
+    log.debug(f"parse condition - {condition}")
+    condition = condition.lower()
+    if condition in locale.condition_types:
+        return locale.condition_types[condition]
+    elif condition in locale.condition_synonyms:
+        return locale.condition_types[locale.condition_synonyms[condition]]
+    else:
+        return ConditionType.UNKNOWN
+
+
+def parse_item(item: str, locale):
+    log.debug(f"parse item - {item}")
+    items = locale.rain_items + locale.cold_items + locale.warm_items + locale.sun_items
+    items_lowercase = [x.lower() for x in items]
+    if item.lower() in items_lowercase:
+        return items[items_lowercase.index(item.lower())]
+
+
+def parse_temperature(temperature: str, locale):
+    log.debug(f"parse temperature - {temperature}")
+    if temperature in locale.temperature_types:
+        return locale.temperature_types[temperature]
+    elif temperature in locale.temperature_synonyms:
+        return locale.temperature_types[locale.temperature_synonyms[temperature]]
