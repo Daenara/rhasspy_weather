ddffaf9adcb4f1d8b603d806014be3dea3e51da4
Nadine Seibel <nadine.seibel@googlemail.com>
2020-09-03 12:15:37 +0200

    found a bug in location (empty zipcode was set which ended with an owm error code)

--- rhasspy_weather/data_types/location.py
+++ rhasspy_weather/data_types/location.py
@@ -2,10 +2,10 @@ class Location:
     def __init__(self, city, zipcode=None, country_code=None, lat=None, lon=None):
         self.city = city
         self.name = city  # used for output only, intended for custom queries like how is the weather at grandmas (not implemented yet)
-        if not ((lat is None or lon is None) or (lat == "" or lon == "")):
+        if lat and lon:
             self.lat = lat
             self.lon = lon
-        if not (zipcode is None or country_code is None) or (zipcode is "" or country_code is ""):
+        if zipcode and country_code:
             self.zipcode = zipcode
             self.country_code = country_code
 
