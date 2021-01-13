2778ad2093e626ea126a198f5717b170e74977b8
Daenara <nadine@daenara.net>
2021-01-13 13:57:29 +0100

    added more template values

--- rhasspy_weather/templates.py
+++ rhasspy_weather/templates.py
@@ -8,6 +8,7 @@ from string import Template
 from rhasspy_weather.data_types.config import get_config
 from rhasspy_weather.data_types.location import Location
 from rhasspy_weather.data_types.error import WeatherError
+from rhasspy_weather.data_types.report import WeatherReport
 
 log = logging.getLogger(__name__)
 
@@ -26,9 +27,7 @@ def fill_template(weather_input, result, template_override=None, remove_not_repl
         template_values = {**template_values, **weather_error_to_template_values(result)}
     else:
         template_values = {**template_values, **weather_report_to_template_values(result), **weather_object_to_template_values(result.request, "request")}
-    if "rhasspy_intent" in config.parser.__name__:
-        template_values = {**template_values, **intent_to_template_values(weather_input)}
-
+    template_values = {**template_values, **config.parser.get_template_values(weather_input)}
     output = template.safe_substitute(template_values)
     if "\n" in output and remove_not_replaced_lines:
         output_array = output.splitlines()
@@ -58,42 +57,49 @@ def intent_to_template_values(intent_message):
             template_values["intent_" + key] = value
     return template_values
 
+__template_values = {}
+__report_speech = None
 
-__report = None
 
-
-def weather_report_to_template_values(report):
-    global __report
-    if __report is None or __report != report.speech[report.request.forecast_type]:
-        __report = report.speech[report.request.forecast_type]
+def weather_report_to_template_values(report: WeatherReport) -> dict:
+    global __report_speech
+    if __report_speech is None or __report_speech != report.speech[report.request.forecast_type]:
+        __report_speech = report.speech[report.request.forecast_type]
     template_values = {
-        "weather_text": __report
+        "speech": __report_speech
+    }
+    template_values = {**template_values, ** weather_object_to_template_values(report, "report")}
+    return template_values
+
+
+def weather_error_to_template_values(error: WeatherError) -> dict:
+    template_values = {
+        "speech": error.message
     }
     return template_values
 
 
-def weather_error_to_template_values(error):
-    template_values = {
-        "weather_text": error.message
-    }
-    return template_values
-
-
-def weather_object_to_template_values(weather_object, name):
-    template_values = {}
-    for key, value in weather_object.__dict__.items():
-        new_key = name + "_" + key.replace("_" + type(weather_object).__name__ + "__", "")
-        if isinstance(value, str) and not value == "":
-            template_values[new_key] = value
-        elif isinstance(value, bool):
-            template_values[new_key] = value
-        elif isinstance(value, Enum):
-            template_values[new_key] = str(value)
-        elif isinstance(value, datetime.time) or isinstance(value, datetime.date):
-            template_values[new_key] = str(value)
-        elif isinstance(value, Location):
-            for l_key, l_value in value.__dict__.items():
-                new_l_key = new_key + "_" + l_key
-                template_values[new_l_key] = l_value
-    return template_values
+def weather_object_to_template_values(weather_object, name) -> dict:
+    global __template_values
+    if name not in __template_values:
+        __template_values[name] = {}
+        for key, value in weather_object.__dict__.items():
+            new_key = name + "_" + key.replace("_" + type(weather_object).__name__ + "__", "")
+            if isinstance(value, str) and not value == "":
+                __template_values[name][new_key] = value
+            elif isinstance(value, bool):
+                __template_values[name][new_key] = value
+            elif isinstance(value, int):
+                __template_values[name][new_key] = value
+            elif isinstance(value, float):
+                __template_values[name][new_key] = value
+            elif isinstance(value, Enum):
+                __template_values[name][new_key] = str(value)
+            elif isinstance(value, datetime.time) or isinstance(value, datetime.date):
+                __template_values[name][new_key] = str(value)
+            elif isinstance(value, Location):
+                for l_key, l_value in value.__dict__.items():
+                    new_l_key = new_key + "_" + l_key
+                    __template_values[name][new_l_key] = l_value
+    return __template_values[name]
 
